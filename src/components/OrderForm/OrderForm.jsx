66aa5a414fef1e12823cc794ec4e36ae9f809bc6
Chaooohs <01nikka01@gmail.com>
2024-07-17 10:48:05 +0300

    second form

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import { orderComplited, orderGet } from "../../redux/orderSlice";
-import { useState } from "react";
 
 import styles from './OrderForm.module.scss'
 
@@ -10,24 +9,20 @@ export const OrderForm = () => {
   const totalPrice = useSelector((state) => state.cart.totalPrice);
   const products = useSelector((state) => state.cart.products);
 
-  const [phone, setPhone] = useState('')
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-  const [city, setCity] = useState('')
-  const [street, setStreet] = useState('')
-  const [house, setHouse] = useState(0)
-  const [appartment, setAppartment] = useState(0)
 
+  const onHandleForm = (e) => {
+    e.preventDefault()
 
-  const onPhoneChanged = (e) => setPhone(e.target.value)
-  const onEmailChanged = (e) => setEmail(e.target.value)
-  const onNameChanged = (e) => setName(e.target.value)
-  const onCityChanged = (e) => setCity(e.target.value)
-  const onStreerChanged = (e) => setStreet(e.target.value)
-  const onHouseChanged = (e) => setHouse(e.target.value)
-  const onAppartmentChanged = (e) => setAppartment(e.target.value)
+    const data = new FormData(formElem)
 
-  const onSetOrderClick = () => {
+    const phone = data.get('phone')
+    const email = data.get('email')
+    const name = data.get('name')
+    const city = data.get('city')
+    const street = data.get('street')
+    const house = data.get('house')
+    const appartment = data.get('appartment')
+
     if (phone && email && name && city && street && house && appartment) {
 
       const a = {
@@ -48,8 +43,9 @@ export const OrderForm = () => {
     }
   }
 
+
   return (
-    <>
+    <form id="formElem" >
       <h1 className='text-chapter'>Order</h1>
       <p className='text-id' style={{ fontWeight: 'bold' }}>Contact information</p>
       <div className={styles.box}>
@@ -58,21 +54,18 @@ export const OrderForm = () => {
           type="text"
           placeholder='Phone'
           name='phone'
-          onChange={onPhoneChanged}
         />
         <input
           className={styles.input}
           type="text"
           placeholder='Email'
           name='email'
-          onChange={onEmailChanged}
         />
         <input
           className={styles.input}
           type="text"
           placeholder='Name'
           name='name'
-          onChange={onNameChanged}
         />
       </div>
       <p className='text-id' style={{ fontWeight: 'bold' }}>Address</p>
@@ -82,14 +75,12 @@ export const OrderForm = () => {
           type="text"
           placeholder='City'
           name='city'
-          onChange={onCityChanged}
         />
         <input
           className={styles.input}
           type="text"
           placeholder='Street'
           name='street'
-          onChange={onStreerChanged}
         />
         <div className={styles.house}>
           <div className={styles.layoutlabel}>
@@ -99,7 +90,6 @@ export const OrderForm = () => {
               id='el1'
               type="numger"
               name='house'
-              onChange={onHouseChanged}
             />
           </div>
           <div className={styles.layoutlabel}>
@@ -109,7 +99,6 @@ export const OrderForm = () => {
               id='el2'
               type="numger"
               name='appartment'
-              onChange={onAppartmentChanged}
             />
           </div>
         </div>
@@ -117,12 +106,13 @@ export const OrderForm = () => {
           <span className='text-chapter'>Total Price:</span>
           <span className='text-chapter'>{`${totalPrice} $`}</span>
         </div>
-        <button
+        <input
+          type="submit"
+          value={'Ordered'}
           className={styles.buttonorder}
-          onClick={onSetOrderClick}
-        >
-          Ordered</button>
+          onClick={onHandleForm}
+        />
       </div>
-    </>
+    </form>
   )
 }
